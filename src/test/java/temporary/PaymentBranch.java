ed84dc4c4187dc775957dbfea8637a2b19183069
automationa2z <62273665+automationa2z@users.noreply.github.com>
2024-01-06 22:46:17 -0500

    Update PaymentBranch.java

--- src/test/java/temporary/PaymentBranch.java
+++ src/test/java/temporary/PaymentBranch.java
@@ -1,4 +1,8 @@
 public class PaymentBranch{
+  public void sayHello(){
+    System.out.println("Hello How are you?");
+  }
+  
   public static void main(String[] args){
     System.out.println("Chenges made on Paymentfunc branch for demo purpose");
   }
