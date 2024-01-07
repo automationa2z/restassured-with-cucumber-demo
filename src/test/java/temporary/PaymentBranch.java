79ba1b300adeb06cb676f1a6ba2f4f4f495d9afd
automationa2z <62273665+automationa2z@users.noreply.github.com>
2024-01-07 14:45:52 -0500

    Update PaymentBranch.java - adding askName() method

--- src/test/java/temporary/PaymentBranch.java
+++ src/test/java/temporary/PaymentBranch.java
@@ -4,6 +4,10 @@ public class PaymentBranch {
     System.out.println("Hello How are you?");
   }
 
+  public void askName(){
+    System.out.println("What's your name?");
+  }
+
   public static void main(String[] args) {
     System.out.println("Chenges made on Paymentfunc branch for demo purpose");
   }
